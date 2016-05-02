ff1a48812e580810bfeacc1c090f5d8f9594a9b4
HamadH <hmyhafiz@gmail.com>
2016-05-02 13:49:36 -0400

    Added files via upload

---
+++ AngularJsApp/js/app.js
@@ -0,0 +1,38 @@
+﻿
+var myApp = angular.module('myApp', [
+    'ngRoute',
+    'artistControllers',
+    'ngGrid'
+]);
+
+myApp.config(['$routeProvider', function ($routeProvider) {
+   // alert("hhhhhhhhhhh");
+    $routeProvider.   
+        when('/list', {
+
+        templateUrl: 'partials/list.html',
+        controller: 'ListController'
+    }).
+
+
+         when('/details/:itemId', {
+
+             templateUrl: 'partials/details.html',
+             controller: 'DetailsController'
+         }).
+
+           when('/grid', {
+
+               templateUrl: 'http://localhost:53077/AngularJsApp/partials/GrigPage.html',
+               controller: 'MyCtrl'
+           }).
+    otherwise({
+        redirectTo:'/list'
+    });
+
+}]);
+
+
+
+
+
